e8819539347628a21e741661ef597b2a54d76ced
1391819 <roberto.nacu@gmail.com>
2023-07-29 00:57:37 +0100

    added comments

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,12 @@
+# imports
 from flask import Flask
 from classes.library import Library
 
+# creating Flask instance
 app = Flask(__name__)
 
 # creating empty library
 library = Library()
 
+# importing view functions after Flask instance creation
 import application.routes
